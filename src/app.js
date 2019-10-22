c624a84d8d676e177a87f4bb09c1d4fe3753926f
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-10-22 19:54:04 -0300

    connected mongodb

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
+
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
 
-// importing routes
-const indexRoutes = require ('./routes/index');
+// connection to db
+mongoose.connect('mongodb://localhost/crud-mongo')
+  .then(db => console.log('db connected'))
+  .catch(err => console.log(err));
 
+// importing routes
+const indexRoutes = require('./routes/index');
 
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views')); //nos muestra todo el lugar donde se encuentra en este caso /views
-app.set('view engine','ejs'); //Mostramos un motor de plantilla (ejs)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+// routes
+app.use('/', indexRoutes);
 
-// Routes
-
-
-// starting the server 
 app.listen(app.get('port'), () => {
-    console.log(`server on port ${app.get('port')}`);
-  });
+  console.log(`server on port ${app.get('port')}`);
+});     
