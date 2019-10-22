26487f5220c9f7884d3c063abcb82d5688415577
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-10-22 19:32:13 -0300

    added index.js in the folder "Routes"

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const app = express();
+
 // importing routes
+const indexRoutes = require ('./routes/index');
+
 
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views')); //nos muestra todo el lugar donde se encuentra en este caso /views
 app.set('view engine','ejs'); //Mostramos un motor de plantilla (ejs)
+
 // middlewares
 app.use(morgan('dev'));
 
