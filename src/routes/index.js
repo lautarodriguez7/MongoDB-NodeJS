5e4f4cc9cc0a6928c6a1345ca6b7fad392c96bd3
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-10-22 23:56:21 -0300

    adding multitask

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res) => {
-    res.render('index');
+const Task = require('../models/task');
+
+router.get('/', async(req, res) => {
+    const tasks = await Task.find();
+    res.render('index', {
+        tasks // == tasks:tasks
+    });
 });
+
+router.post('/add', async (req, res, next) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
+  });
+
+  
 
 module.exports = router; 
